bb1f707ec4a4f79f0e210303d1ea6560bc823417
Dimanok1989 <kolgaev@gmail.com>
2025-09-29 23:47:04 +0300

    git workflow init

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [
+        {
+            name: 'kolgaev-pdd',
+            script: 'node_modules/next/dist/bin/next',
+            args: '-p 3011'
+        },
+    ],
+};
